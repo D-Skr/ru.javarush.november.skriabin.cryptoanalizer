6f2a669753a9d8a4d272ad3a6f43d3e431065368
Di <altusj@gmail.com>
2022-10-13 00:05:18 -0400

    change relative to absolute path

--- view/OptionEncryptFile.java
+++ view/OptionEncryptFile.java
@@ -17,11 +17,10 @@ public class OptionEncryptFile {
 		static void run(List<Character> alphabet) throws IOException {
 			long currentTime = System.currentTimeMillis();
 			Scanner console = new Scanner(System.in);
-			System.out.println("Please copy your file into 'input' folder");
-			System.out.println("Please enter the name of your text file:");
-			String pathNotEncryptedFile = "input/" + console.nextLine();
-			System.out.println(pathNotEncryptedFile);
-			String pathEncryptedFile = "output/encrypted" + currentTime + ".txt";
+			System.out.println("Please enter the absolute path and name of your text file:");
+			String pathNotEncryptedFile = console.nextLine();
+			String pathEncryptedFile = pathNotEncryptedFile.substring(0, pathNotEncryptedFile.length() - 4) + "_encrypted_"
+					+ currentTime + ".txt";
 			System.out.println("Please enter shift key:");
 			int shift = Integer.parseInt(console.nextLine());
 			System.out.println("Your result saved in: " + pathEncryptedFile);
